e8d936b042ca20fd451bdb7d43f3a74b66f07cce
Evgeniya Avksenteva <evgesh-092009@yandex.ru>
2025-05-22 22:18:29 +0300

    HW 21_2 html

---
+++ src/path.py
@@ -0,0 +1,13 @@
+import os
+
+
+def root_join(*parts):
+    """ Функция возвращающая путь к файлу указанному из корня """
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    full_path = os.path.join(base_dir, *parts)
+    return full_path
+
+
+if __name__ == '__main__':
+    with open(root_join('html_pages', 'contacts.html'), encoding='utf-8') as f:
+        content = f.read()
